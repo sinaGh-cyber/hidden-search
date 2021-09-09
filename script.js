7b1049dc8413c0c5e223d256cc78e2764e42d1ee
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-09-09 07:13:04 +0430

    js ready
    final

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const search = document.querySelector('.search');
+const button = document.getElementsByTagName('button')[0];
+const input = document.querySelector('.input');
 
+button.addEventListener('click', () => {
+  if (input.value) {
+    window.location.assign(`http://www.google.com/search?q=${input.value}`);
+    input.value = '';
+    return;
+  }
+  search.classList.toggle('active');
+});
